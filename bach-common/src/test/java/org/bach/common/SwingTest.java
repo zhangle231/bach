815d3156d8b19910cac8c8794e4f370dfaa66de9
ricky <ricky@DESKTOP-K0GDIH4>
2019-04-30 06:56:44 +0800

    swing test

---
+++ bach-common/src/test/java/org/bach/common/SwingTest.java
@@ -0,0 +1,14 @@
+package org.bach.common;
+
+import javax.swing.JFrame;
+
+public class SwingTest extends JFrame {
+
+	public static void main(String[] args) {
+		SwingTest frame = new SwingTest();
+		frame.setSize(500 , 1000);
+		frame.setVisible(true);
+
+	}
+
+}

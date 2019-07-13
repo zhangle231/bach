3f6d0e7484e1a3ae07c52c16cb7df2bc48461c8d
leo <leo@leo>
2019-07-13 20:34:23 +0800

    new commit

--- bach-cloud/bach-cloud-ms-simple-consumer-movie/src/main/java/org/bach/cloud/ms/simple/consumer/movie/App.java
+++ bach-cloud/bach-cloud-ms-simple-consumer-movie/src/main/java/org/bach/cloud/ms/simple/consumer/movie/App.java
@@ -2,9 +2,11 @@ package org.bach.cloud.ms.simple.consumer.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
 
